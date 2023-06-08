64b43dbc252d1b3fd33c945e77944473dcc6e73d
Nguyễn Đặng Tuấn Kiệt <tuankietnk2001@gmail.com>
2023-06-09 03:01:34 +0700

    Change upload image mimetype to png if it was jpeg

--- helpers/uploaders/item_image.uploader.js
+++ helpers/uploaders/item_image.uploader.js
@@ -1,7 +1,7 @@
 const multer = require('multer')
 const path = require('path')
-
 const fs = require('fs')
+
 const destination = process.cwd() + '/public/images/items'
 
 if (!fs.existsSync(destination)) {
@@ -13,12 +13,22 @@ const storage = multer.diskStorage({
         cb(null, destination)
     },
     filename: function (request, file, cb) {
+        // Change the file extension to '.png' if it was '.jpeg' or '.jpg'
+        const fileExt = path.extname(file.originalname)
+        if (fileExt === '.jpeg' || fileExt === '.jpg') {
+            file.originalname = file.originalname.replace(fileExt, '.png')
+        }
+
         cb(null, request.params.id + path.extname(file.originalname))
     },
 })
 
 const fileFilter = (request, file, cb) => {
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    if (file.mimetype === 'image/jpeg') {
+        file.mimetype = 'image/png'
+    }
+
+    if (file.mimetype === 'image/png') {
         cb(null, true)
     } else {
         cb(new Error('Unsupported file type'), false)
