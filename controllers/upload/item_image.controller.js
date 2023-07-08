fa4564d8a51f9c74381ddb529df9332545318442
kietndt-0211 <87163945+tuankiet-0904@users.noreply.github.com>
2023-07-08 15:35:44 +0700

    Implant API remove image background when item image is uploaded

--- controllers/upload/item_image.controller.js
+++ controllers/upload/item_image.controller.js
@@ -1,7 +1,32 @@
+const fs = require('fs')
 const path = require('path')
 const axios = require('axios')
+const FormData = require('form-data')
 
 const { getItemById, updateItemById } = require('../CRUD/item')
+
+async function fetchRemoveImageBackground(imageUrl) {
+    const formData = new FormData();
+    formData.append('size', 'auto');
+    formData.append('image_file', fs.createReadStream(imageUrl), path.basename(imageUrl));
+
+    try {
+        const removeBackgroundResponse = await axios.post('https://api.remove.bg/v1.0/removebg', formData, {
+            responseType: 'arraybuffer',
+            headers: {
+                ...formData.getHeaders(),
+                'X-Api-Key': 'Sq7F5cEhQmuqSm2eSaZDW8Yr',
+            },
+            encoding: null
+        })
+
+        if (removeBackgroundResponse.status === 200) {
+            fs.writeFileSync(imageUrl, removeBackgroundResponse.data);
+        }
+    } catch (error) {
+        console.log('Request remove background failed:', error)
+    }
+}
 
 async function uploadSingle(request, response) {
     try {
@@ -17,6 +42,9 @@ async function uploadSingle(request, response) {
                     image: imageUrl,
                 }
 
+                // Call remove background API
+                await fetchRemoveImageBackground(imageUrl)
+                
                 // Call flask server API to extract new image features
                 axios.post(
                     `${process.env.AI_SERVER_URL}/extract-new-item-image-features`,
